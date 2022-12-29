8da339ec341b054e65361b37cab62b77293c3280
Nick Sollon <genericjoke@gmail.com>
2022-12-29 15:28:44 -0500

    commit tree fbe0a4624b85de3059c848bf8d231c3f09b00343

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+function appendCat(name){
+    const temp = [...cats, name];
+    return temp;
+}
+function prependCat(name){
+    const temp =[name, ...cats];
+    return temp;
+}
+function removeLastCat(){
+    return cats.slice(0, cats.length-1);
+}
+function removeFirstCat(){
+    return cats.slice(1,cats.length);
+}
